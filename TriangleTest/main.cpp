c89f931c3e16c9ff1e14da445a53ced194ef7ae8
tung.nguyen <tung.nguyen@SEA-8E3F0F>
2016-08-31 15:15:28 -0700

    Updated

--- TriangleTest/main.cpp
+++ TriangleTest/main.cpp
@@ -11,6 +11,11 @@ int main()
 
 	window.Init(800, 600, "I got a title :^)");
 
+	Vertex verts[] = { { 1,1,0,1 },{ 1,-1,0,1 },{ -1,-1,0,1 },{ -1,1,0,1 } };
+	unsigned tris[] = { 0,1,2, 2,3,0 };
+
+	gallery.MakeObject("quad", verts, 4, tris, 6);
+
 	gallery.LoadObjectOBJ("Sphere", "../res/models/sphere.obj");
 	gallery.LoadShader("Simple", "../res/shaders/SimpleVert.txt", "../res/shaders/SimpleFrag.txt");
 
@@ -19,7 +24,8 @@ int main()
 	while (window.Step())
 	{
 		time += 0.0016667f;
-		Draw(gallery.GetShader("Simple"), gallery.GetObject("Sphere"), time);
+		//Draw(gallery.GetShader("Simple"), gallery.GetObject("Sphere"), time);
+		Draw(gallery.GetShader("Simple"), gallery.GetObject("quad"), time);
 	}
 
 	gallery.Term();
