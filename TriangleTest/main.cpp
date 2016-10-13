5e61ef2fb697ae658c7b0a29ee22a9ef8c504bf7
tung.nguyen <tung.nguyen@SEA-8E3F0F>
2016-10-13 12:02:14 -0700

    Added assessment

--- TriangleTest/main.cpp
+++ TriangleTest/main.cpp
@@ -42,6 +42,8 @@ void main()
 	Geometry quad = MakeGeometry(quad_verts, 4, quad_tris, 6);
 	Geometry spear = LoadObj("../res/models/soulspear.obj");
 	Geometry sphere = LoadObj("../res/models/sphere.obj");
+	Geometry plane = GenGrid(516, 20);
+	Geometry plane2 = GenGrid(512, 20);
 
 	//Generated objects
 	//Texture noise = GenRadialGradient(64, glm::vec4(1, 0, 0, 1), glm::vec4(0, 0, 1, 1), 0.5f);
@@ -51,6 +53,8 @@ void main()
 	Texture spear_normal = LoadTex("../res/textures/soulspear_normal.tga");
 	Texture spear_diffuse = LoadTex("../res/textures/soulspear_diffuse.tga");
 	Texture spear_specular = LoadTex("../res/textures/soulspear_specular.tga");
+	Texture heighttex = LoadTex("../res/textures/height.jpg");
+	Texture meleetex = LoadTex("../res/textures/melee.jpg");
 
 	//Texture combine
 	Texture spear_Textures[3] = { spear_normal, spear_diffuse, spear_specular };
@@ -98,6 +102,9 @@ void main()
 	glm::mat4 spearModel;
 	glm::mat4 sphereModel = glm::translate(glm::vec3(0.3f, -1, -0.2f));
 	glm::mat4 wallModel = glm::rotate(45.f, glm::vec3(0, -1, 0)) * glm::translate(glm::vec3(0, 0, -2)) * glm::scale(glm::vec3(2, 2, 1));
+	glm::mat4 planeModel = glm::translate(glm::vec3(0, -2, 0));
+	glm::mat4 planeModel2 = glm::translate(glm::vec3(-20, -2, 0));
+	glm::mat4 meleeModel = glm::translate(glm::vec3(-40, -2, 0));
 
 
 	//Light matrix//
@@ -106,19 +113,85 @@ void main()
 	glm::vec4 redColor = glm::vec4(1, 0, 0, 1);
 	glm::mat4 greenView = glm::lookAt(glm::normalize(-glm::vec3(1, 1, -1)), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 	glm::vec4 greenColor = glm::vec4(0, 1, 0, 1);
+	glm::mat4 blueView = glm::lookAt(glm::normalize(-glm::vec3(1, 1, -1)), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::vec4 blueColor = glm::vec4(0, 0, 1, 1);
+	glm::mat4 whiteView = glm::lookAt(glm::normalize(-glm::vec3(1, 1, -1)), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::vec4 whiteColor = glm::vec4(1, 1, 1, 1);
 
+	//Camera
+	FlyCamera cam;
+	cam.JumpTo(glm::vec3(0, 0, -10));
+	//cam.LookAt(glm::vec3(0, 0, 0));
+
+
+	//Post process settings
+	int changeType = 1; //1 = saturation, 2 = brightness, 3 = contrast
+	float saturation = 0.5f;
+	float brightness = 1;
+	float contrast = 1;
+	float mountainScale = 1;
+	float mountainX = 0.5f;
+	float mountainY = 0.5f;
 
 	while (context.Step())
 	{
 		time.Step();
 		input.Step();
+
+		//Value input
+		if (input.GetKeyState('1') == Input::RELEASE) changeType = 1;
+		if (input.GetKeyState('2') == Input::RELEASE) changeType = 2;
+		if (input.GetKeyState('3') == Input::RELEASE) changeType = 3;
+		if (input.GetKeyState('4') == Input::RELEASE) changeType = 4;
+		if (input.GetKeyState('5') == Input::RELEASE) changeType = 5;
+		if (input.GetKeyState('6') == Input::RELEASE) changeType = 6;
+
+		//Just for realtime changes
+		switch (changeType)
+		{
+		case 1:
+			if (input.GetKeyState('-') == Input::DOWN) saturation -= 2 * time.GetDeltaTime();
+			if (input.GetKeyState('=') == Input::DOWN) saturation += 2 * time.GetDeltaTime();
+			break;
+		case 2:
+			if (input.GetKeyState('-') == Input::DOWN) brightness -= 2 * time.GetDeltaTime();
+			if (input.GetKeyState('=') == Input::DOWN) brightness += 2 * time.GetDeltaTime();
+			break;
+		case 3:
+			if (input.GetKeyState('-') == Input::DOWN) contrast -= 2 * time.GetDeltaTime();
+			if (input.GetKeyState('=') == Input::DOWN) contrast += 2 * time.GetDeltaTime();
+			break;
+		case 4:
+			if (input.GetKeyState('-') == Input::DOWN) mountainScale -= 2 * time.GetDeltaTime();
+			if (input.GetKeyState('=') == Input::DOWN) mountainScale += 2 * time.GetDeltaTime();
+			break;
+		case 5:
+			if (input.GetKeyState('-') == Input::DOWN) mountainX -= 2 * time.GetDeltaTime();
+			if (input.GetKeyState('=') == Input::DOWN) mountainX += 2 * time.GetDeltaTime();
+			break;
+		case 6:
+			if (input.GetKeyState('-') == Input::DOWN) mountainY -= 2 * time.GetDeltaTime();
+			if (input.GetKeyState('=') == Input::DOWN) mountainY += 2 * time.GetDeltaTime();
+			break;
+		default:
+			break;
+		}
+
+
 		spearModel = glm::rotate(time.GetTotalTime(), glm::vec3(0, 1, 0)) * glm::translate(glm::vec3(0, -1, 0)) * glm::scale(glm::vec3(1, 1, 1));
+
+		camView = cam.GetView();
+		camProj = cam.GetProjection();
+		cam.Update(input, time);
 
 		//Geometry pass//
 		ClearFramebuffer(geometryFrame);
-		Tdraw(gpass, spear, geometryFrame, spearModel, camView, camProj, spear_diffuse, spear_normal, spear_specular);
-		Tdraw(gpass, sphere, geometryFrame, sphereModel, camView, camProj, white, vertex_normals, white);
-		Tdraw(gpass, quad, geometryFrame, wallModel, camView, camProj, white, vertex_normals, white);
+		Tdraw(gpass, spear, geometryFrame, spearModel, camView, camProj, spear_diffuse, spear_normal, spear_specular, 0);
+		Tdraw(gpass, sphere, geometryFrame, sphereModel, camView, camProj, white, vertex_normals, white, 0);
+		Tdraw(gpass, quad, geometryFrame, wallModel, camView, camProj, white, vertex_normals, white, 0);
+		Tdraw(gpass, plane, geometryFrame, planeModel, camView, camProj, white, vertex_normals, white, 1, mountainScale, mountainX, mountainY);
+		Tdraw(gpass, plane2, geometryFrame, planeModel2, camView, camProj, heighttex, vertex_normals, white, 2, mountainScale, mountainX, mountainY, heighttex);
+		Tdraw(gpass, plane2, geometryFrame, meleeModel, camView, camProj, meleetex, vertex_normals, white, 2, mountainScale, mountainX, mountainY, meleetex);
 
 
 		//Light pass//
@@ -129,6 +202,8 @@ void main()
 		Tdraw(spass, spear, shadowFrame, spearModel, redView, lightProj);
 		Tdraw(spass, sphere, shadowFrame, sphereModel, redView, lightProj);
 		Tdraw(spass, quad, shadowFrame, wallModel, redView, lightProj);
+		//Tdraw(spass, plane, shadowFrame, planeModel, redView, lightProj);
+		//Tdraw(spass, plane2, shadowFrame, planeModel2, redView, lightProj);
 		//Light aggregation
 		Tdraw(lpass, quad, lightFrame, camView,
 			geometryFrame.colors[0], geometryFrame.colors[1], geometryFrame.colors[2], geometryFrame.colors[3],
@@ -139,28 +214,54 @@ void main()
 		Tdraw(spass, spear, shadowFrame, spearModel, greenView, lightProj);
 		Tdraw(spass, sphere, shadowFrame, sphereModel, greenView, lightProj);
 		Tdraw(spass, quad, shadowFrame, wallModel, greenView, lightProj);
+		//Tdraw(spass, plane, shadowFrame, planeModel, greenView, lightProj);
+		//Tdraw(spass, plane2, shadowFrame, planeModel2, greenView, lightProj);
 		//add the green light now.
 		Tdraw(lpass, quad, lightFrame, camView,
 			geometryFrame.colors[0], geometryFrame.colors[1], geometryFrame.colors[2], geometryFrame.colors[3],
 			shadowFrame.depth, greenColor, greenView, lightProj);
 
+		//Blue light
+		//ClearFramebuffer(shadowFrame);
+		//Tdraw(spass, spear, shadowFrame, spearModel, blueView, lightProj);
+		//Tdraw(spass, sphere, shadowFrame, sphereModel, blueView, lightProj);
+		//Tdraw(spass, quad, shadowFrame, wallModel, blueView, lightProj);
+		////Tdraw(spass, plane, shadowFrame, planeModel, blueView, lightProj);
+		////add the blue light now.
+		//Tdraw(lpass, quad, lightFrame, camView,
+		//	geometryFrame.colors[0], geometryFrame.colors[1], geometryFrame.colors[2], geometryFrame.colors[3],
+		//	shadowFrame.depth, blueColor, blueView, lightProj);
+
+		//Whites light
+		//To do: add a light map
+		//ClearFramebuffer(shadowFrame);
+		//Tdraw(spass, spear, shadowFrame, spearModel, whiteView, lightProj);
+		//Tdraw(spass, sphere, shadowFrame, sphereModel, whiteView, lightProj);
+		//Tdraw(spass, quad, shadowFrame, wallModel, whiteView, lightProj);
+		////Tdraw(spass, plane, shadowFrame, planeModel, whiteView, lightProj);
+		////add the blue light now.
+		//Tdraw(lpass, quad, lightFrame, camView,
+		//	geometryFrame.colors[0], geometryFrame.colors[1], geometryFrame.colors[2], geometryFrame.colors[3],
+		//	shadowFrame.depth, whiteColor, whiteView, lightProj);
+
 
 		//Post pass//
 		ClearFramebuffer(postFrame); 
-		Tdraw(postShader, quad, postFrame, lightFrame.colors[0], 1.0f, 1.0f, 1.0f, glm::vec4(1, 0, 0, 1), glm::vec4(0, 0, 1, 1), 0.5f);
+		Tdraw(postShader, quad, postFrame, lightFrame.colors[0], saturation, brightness, contrast, glm::vec4(1, 0, 0, 1), glm::vec4(0, 0, 1, 1), 0.5f);
 
 		//Test
 		//Debug windows
-		Texture debug_list[] = { geometryFrame.colors[0], geometryFrame.colors[1], geometryFrame.colors[2], geometryFrame.colors[3], geometryFrame.depth, lightFrame.colors[1], lightFrame.colors[2], shadowFrame.depth};
+		/*Texture debug_list[] = { geometryFrame.colors[0], geometryFrame.colors[1], geometryFrame.colors[2], geometryFrame.colors[3], geometryFrame.depth, lightFrame.colors[1], lightFrame.colors[2], shadowFrame.depth};
 		for (int i = 0; i < sizeof(debug_list) / sizeof(Texture); ++i)
 		{
 			glm::mat4 mod = glm::translate(glm::vec3(-.75f + .5f*(i % 4), 0.75f - .5f*(i / 4), 0)) * glm::scale(glm::vec3(0.25f, 0.25f, 1.f));
 			Tdraw(qdraw, quad, screen, debug_list[i], mod);
-		}
+		}*/
 
 
 		//Final Product
-		glm::mat4 mod = glm::translate(glm::vec3(-.5f, -0.5f, 0)) * glm::scale(glm::vec3(0.5f, 0.5f, 1.f));
+		//glm::mat4 mod = glm::translate(glm::vec3(-.5f, -0.5f, 0)) * glm::scale(glm::vec3(0.5f, 0.5f, 1.f));
+		glm::mat4 mod = glm::translate(glm::vec3(0, 0, 0)) * glm::scale(glm::vec3(1.0f, 1.0f, 1.f));
 		Tdraw(qdraw, quad, screen, postFrame.colors[0], mod);
 	}
 
--- res/shaders/GPass.vert
+++ res/shaders/GPass.vert
@@ -8,17 +8,48 @@ layout(location = 0) uniform mat4 model;
 layout(location = 1) uniform mat4 view;
 layout(location = 2) uniform mat4 proj;
 
-layout(location = 6) uniform bool shapeMod;
+layout(location = 6) uniform int shapeType; //0 = regular, 1 = code generated height map, 2 = height map from file
+layout(location = 7) uniform float mountainScale;
+layout(location = 8) uniform float mountainX;
+layout(location = 9) uniform float mountainY;
+layout(location = 10) uniform sampler2D heightMap;
 
 out vec2 vUV;
 out vec3 vNormal;
 out vec3 vPosition;
+
+vec4 lerp(vec4 start, vec4 target, float percentage)
+{
+	return start * (1.0f - percentage) + target * percentage;
+}
+
+float Distance(vec2 a, vec2 b)
+{
+	vec2 thedistance = a - b;
+	float magnitude = sqrt(thedistance.x*thedistance.x + thedistance.y*thedistance.y);
+	return magnitude;
+}
 
 void main()
 {
 	vNormal   = (view * model * normal).xyz;
 	vPosition = (view * model * position).xyz;
 	vUV = texcoord;
+	if(shapeType == 1)
+	{
+		//Generates a radial gradient heightmap from white to black, uses red to determine the height
+		vec2 CenterPosition = vec2(mountainX, mountainY);
+		float t = Distance(texcoord, CenterPosition) * 1.41421356237;
+		vec4 p = position;
+		p.g = lerp(vec4(1, 1, 1, 1), vec4(0, 0, 0, 1), t + (0.5 - 0.5) * 2).r * mountainScale;
 
-	gl_Position = proj * view * model * position;
+		gl_Position = proj * view * model * p;
+	}
+	else if(shapeType == 2)
+	{
+		vec4 p = position;
+		p.g = texture(heightMap, texcoord).r * mountainScale;
+		gl_Position = proj * view * model * p;
+	}
+	else gl_Position = proj * view * model * position;
 }
